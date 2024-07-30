4f3267a376d62dfb454440ddf5a37d23d26deb4d
iamjhonatan <jhonatanmedeiros7@gmail.com>
2024-07-30 20:23:47 -0300

    feat: configuração inicial

---
+++ ApiIdentityEndpoint/Program.cs
@@ -0,0 +1,25 @@
+using ApiIdentityEndpoint.Data;
+using ApiIdentityEndpoint.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddDbContext<AppDbContext>(options => 
+                                    options.UseSqlServer("Server=localhost;Database=AspNetIdentityEndpoint;Trusted_Connection=True;encrypt=false")
+    );
+
+// builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
+builder.Services
+    .AddIdentityApiEndpoints<User>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapIdentityApi<User>();
+
+app.Run();
